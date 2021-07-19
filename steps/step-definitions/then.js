bb0625dc5b22527126c03f695c8ff7014b8af89e
shreya390 <67576673+shreya390@users.noreply.github.com>
2021-07-19 17:13:36 +0530

    Add files via upload

---
+++ steps/step-definitions/then.js
@@ -0,0 +1,15 @@
+const {Then } = require('@cucumber/cucumber');
+const { assert } = require('chai');
+Then ("expect new tab opened {string}",(no_of_tabs) =>{
+    const  handles = browser.getWindowHandles();
+    no_of_tabs=handles.length
+    console.log("no of tabs opened ",no_of_tabs);
+    assert.ok(no_of_tabs==2);
+});
+
+Then("I expect the elements {string} position  is not equal to  the saved position",(locator)=>{
+    var comic_card=$(locator);
+    var after_location=Number(comic_card.getLocation('y'));
+    console.log("the position after hovering is ",after_location);
+    assert(before_location!=after_location);
+});
