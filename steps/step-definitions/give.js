a363db86905eb83ad869c625c6cc35b1b2a000fe
shreya390 <67576673+shreya390@users.noreply.github.com>
2021-07-22 17:55:20 +0530

    Update give.js

--- steps/step-definitions/give.js
+++ steps/step-definitions/give.js
@@ -1,11 +1,7 @@
 const { Given} = require('@cucumber/cucumber');
 const { assert } = require('chai');
 
-var no_of_tabs=0;
-var before_location='';
-
 Given("open url {string}",(url) => {
     browser.url(url);
-    browser.maximizeWindow();   
-    expect(url).toEqual("/explore");
+    browser.maximizeWindow();  
 });
